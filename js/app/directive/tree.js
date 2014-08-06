4ad6a5c0227b95fd0f4bcf82f1bca044bd3105db
Virbhadrasinh <virbhadrasinh.18@gmail.com>
2014-08-06 16:50:24 +0530

    Add code comments

--- js/app/directive/tree.js
+++ js/app/directive/tree.js
@@ -1,4 +1,4 @@
-/*This the directive to create parent tree nodes at each level*/
+/*This is the directive to create parent tree nodes at each level*/
 angular.module('TreeCreator').directive('tree', function () {
     "use strict";
     return {
